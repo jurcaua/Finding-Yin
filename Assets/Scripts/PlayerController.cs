50f9b0736b0b3e771b05e5ef7cad8cb9b6439ec7
Alexander Jurcau <jurcaua@gmail.com>
2017-05-13 15:40:07 -0400

    initial commit, basic character controller and shooting with scale down projectiles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5f;
+    public float jumpForce = 5f;
+
+    private bool grounded = true;
+    public Transform groundCheck;
+    private float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    public Transform projectileSpawn;
+    public GameObject projectile;
+    public float projectileSpeed = 10f;
+
+    private Rigidbody2D r;
+    private Vector2 velocity;
+    private int facingRight = 1;
+
+    private SpriteRenderer s;
+
+	void Start () {
+        r = GetComponent<Rigidbody2D>();
+        s = GetComponent <SpriteRenderer> ();
+	}
+	
+	void Update () {
+        if (grounded && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))) {
+            r.AddForce(new Vector2(0f, jumpForce));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject bullet = Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation) as GameObject;
+
+            Rigidbody2D bulletR = bullet.GetComponent<Rigidbody2D>();
+            if (bulletR == null) {
+                Debug.Log("No Rigidbody2D on bullet! Problem!!!");
+                return;
+            }
+
+            bulletR.velocity = new Vector2(facingRight * projectileSpeed, 0f);
+        }
+	}
+
+    void FixedUpdate() {
+        r.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, r.velocity.y);
+
+        if (r.velocity.x != 0f) {
+            facingRight = (int)Mathf.Sign(r.velocity.x);
+            s.flipX = (facingRight == 1) ? false: true ;
+        }
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+    }
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+    public float projectileLifetime = 1f;
+    private float currentLerpTime = 0f;
+
+    private Vector3 startScale;
+
+    void Start () {
+        Destroy(gameObject, projectileLifetime);
+
+        startScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+	
+	void Update () {
+        transform.localScale = Vector3.Lerp(startScale, Vector3.zero, currentLerpTime / projectileLifetime);
+
+        currentLerpTime += Time.deltaTime;
+	}
+}
