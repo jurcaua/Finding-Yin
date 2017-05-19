714c59b21c8855ee4d8653fbb245b1384d7e5eb9
Alexander Jurcau <jurcaua@gmail.com>
2017-05-18 23:09:34 -0400

    basic level selection scene completed, starting on first level

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,11 +6,14 @@ using TMPro;
 public class GameController : MonoBehaviour {
 
     public TextMeshProUGUI text;
+
+    private PlayerController playerController;
     private bool levelComplete = false;
 
 	void Awake () {
+        playerController = GameObject.FindGameObjectWithTag("Yang").GetComponent<PlayerController>();
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/Scripts/SelectLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour {
+
+    public int levelNumber = 1;
+
+	public void ChangeLevel() {
+        SceneManager.LoadScene("level_" + levelNumber.ToString());
+    }
+}
