149e1279ccfa59a296f3e19f7d083dcb73663b66
Alexander Jurcau <jurcaua@gmail.com>
2017-05-28 19:12:52 -0400

    moving clouds, front and back, changing background structure

---
+++ Assets/Scripts/SpriteOffsetMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOffsetMovement : MonoBehaviour {
+
+    public float scrollSpeed = 5f;
+
+    private MeshRenderer m;
+    private float offset;
+    
+	void Start () {
+        m = GetComponent<MeshRenderer>();
+	}
+	
+	void Update () {
+        m.material.mainTextureOffset = new Vector2(Time.time * scrollSpeed, 0f);
+	}
+}
